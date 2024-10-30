613c1bebe8e3a2df1562a90d031dc236a7a7c61f
Darren921 <darren.chung@ontariotechu.net>
2024-10-30 18:07:32 -0400

    Initial commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static Controls controls;
+    private static Vector3 mousePos;
+
+    public static Vector3 GetMousePos()
+    {
+        return mousePos;
+    }
+
+    //Activates player controls;
+    public static void Init(Player player)
+    {
+        controls = new Controls();
+
+        controls.InGame.Movement.performed += _ =>
+        {
+            player.SetMoveDirection(_.ReadValue<Vector3>());
+            player.walkingSound();
+        };
+        controls.InGame.Sprint.performed += _ =>
+        {
+            player.startSprint();
+        };
+        controls.InGame.Sprint.canceled += _ =>
+        {
+            player.cancelSprint();   
+        };
+        controls.InGame.MousePos.performed += _ =>
+        {
+            mousePos = _.ReadValue<Vector2>();
+        };
+        controls.InGame.Hide.performed += _ =>
+        {
+            player.Hide();
+        };
+        controls.InGame.LeftClick.performed += _ =>
+        {
+            player.leftClick();
+        };
+        controls.InGame.Talk.performed += _ =>
+        {
+            player.interactable();
+        };
+        controls.InGame.Talk.canceled += _ =>
+        {
+            player.notInteractable();
+        };
+
+    }
+   
+    public static void EnableInGame()
+    {
+        controls.InGame.Enable();
+    }
+    public static void DisableInGame()
+    {
+        controls.InGame.Disable();
+    }
+
+}
