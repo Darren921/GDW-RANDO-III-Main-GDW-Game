7366687a8e3ad1b10e2facd9a3723c3b7cae0aa6
Darren921 <darren.chung@ontariotechu.net>
2024-11-27 19:28:35 -0500

    spawning pickups started

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,15 +10,19 @@ public class GameManager : MonoBehaviour
     //  [SerializeField]private float Timer;
     // [SerializeField] TextMeshProUGUI TimerDisplay;
     //  [SerializeField] Enemy monsterAI;
-    private GameObject Battery,Fuel;
+    [SerializeField]private GameObject Battery,Fuel;
     private float batteriesInScene,fuelInScene;
     private float minItems;
     private List<GameObject> ItemSpawnPoints;
     private List<int> RandomNum;
     private List<int> discaredNum;
+    bool active;
+
     void Start()
     {
+        minItems = 2;
       //  Boards = GameObject.FindGameObjectsWithTag("Boards");
+      discaredNum = new List<int>();
       ItemSpawnPoints = new List<GameObject>();
       ItemSpawnPoints.AddRange(GameObject.FindGameObjectsWithTag("ItemSpawnPoint"));
         RandomNum = new List<int>();
@@ -26,39 +30,72 @@ public class GameManager : MonoBehaviour
         {
             RandomNum.Add(i);
         }
+
         
-        for (int k = 0; k < ItemSpawnPoints.Count; k++)
-        {        
-            int index = Random.Range(0,RandomNum.Count );
-            int sortednum = RandomNum[index];
-               
-                RandomNum.Remove(sortednum);
-                discaredNum.Add(sortednum);
-        }
 
         /*
         codePanel = FindObjectOfType<CodePanel>();
-        for (int j = 0; j < 4; j++) 
+        for (int j = 0; j < 4; j++)
         {
             comboNums.Add(Random.Range(0,9));
         }
-      
+
         foreach(int values in comboNums)
         {
             print(values);
         }
-        
+
         foreach (var item in comboNums)
         {
             codePanel.comboNumstemp.Add(item);
         }
         */
-      //  Timer = 240;
+        //  Timer = 240;
+    }
+
+    private IEnumerator itemSpawn()
+    {
+        if (!active)
+        {
+            active = true;
+            fuelInScene = GameObject.FindGameObjectsWithTag("Fuel").Length;
+            batteriesInScene = GameObject.FindGameObjectsWithTag("Batteries").Length;
+            if (fuelInScene < minItems)
+            {
+                for (int k = 0; k < ItemSpawnPoints.Count; k++)
+                {
+                    if (RandomNum.Count > 2)
+                    {
+                        int index = Random.Range(0, RandomNum.Count);
+                        int sortednum = RandomNum[index];
+                        discaredNum.Add(sortednum);
+                        RandomNum.Remove(sortednum);
+                    }
+
+                    if (discaredNum.Contains(k))
+                        {
+                            Instantiate(Fuel, ItemSpawnPoints[k].transform.position, Quaternion.identity);
+                        }
+                        if(!discaredNum.Contains(k))
+                        {
+                        }
+                    
+                  
+                }
+            }
+            else if (batteriesInScene < minItems)
+            {
+            
+            }
+        }
+        yield return new WaitForSeconds(5);
+        active = false;
     }
 
 
     void Update()
     {
+        StartCoroutine(itemSpawn());
         /*
         float minutes = Mathf.FloorToInt(Timer / 60f);
         float seconds = Mathf.FloorToInt(Timer - minutes * 60);
