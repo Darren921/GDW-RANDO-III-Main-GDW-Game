b8d774f9b635faea11640ff765113439c093de13
Darren921 <darren.chung@ontariotechu.net>
2025-01-21 19:24:19 -0500

    Organized hierarchy

--- Assets/Scripts/InvSystem/Inv/StaticInterface.cs
+++ Assets/Scripts/InvSystem/Inv/StaticInterface.cs
@@ -1,6 +1,7 @@
     using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class StaticInterface : UserInterface
 {
@@ -11,6 +12,13 @@ public class StaticInterface : UserInterface
         for (int i = 0; i < inventory.Container.Items.Length; i++)
         {
             var obj = slots[i];
+            AddEvent(obj, EventTriggerType.PointerEnter, delegate { OnEnter(obj); });
+            AddEvent(obj, EventTriggerType.PointerExit, delegate { OnExit(obj); });
+            AddEvent(obj, EventTriggerType.BeginDrag, delegate { OnDragStart(obj); });
+            AddEvent(obj, EventTriggerType.EndDrag, delegate { OnDragEnd(obj); });
+            AddEvent(obj, EventTriggerType.Drag, delegate { OnDrag(obj); });
+
+            itemsDisplayed.Add(obj, inventory.Container.Items[i]);;
         }
     }
 }
